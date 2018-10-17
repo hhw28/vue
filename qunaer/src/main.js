f4d532a44276d97abb99d1ff1316e23509036a0a
wanghuan <196912334@qq.com>
2018-10-17 18:29:19 +0800

    add header

--- qunaer/src/main.js
+++ qunaer/src/main.js
@@ -3,9 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import './assets/styles/reset.css'
-import './assets/styles/border.css'
 import fastClick from 'fastClick'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
 
 fastClick.attach(document.body)
 
---
+++ qunaer/src/pages/home/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <span class="iconfont">&#xe624;</span>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点、游玩主题</div>
+    <div class="header-right ">
+      城市
+      <span class="iconfont">&#xe64a;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header
+    line-height: .86rem
+    background: $bgColor
+    display: flex
+    color: #fff
+    .header-left
+      width: .64rem
+      text-align: center
+    .header-input
+      flex: 1
+      line-height: .64rem
+      margin-top: .12rem
+      margin-bottom: .12rem
+      padding-left: .2rem
+      border-radius: .1rem
+      background: #fff
+      color: #ccc
+      .iconfont
+        vertical-align: middle
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
+
+</style>
