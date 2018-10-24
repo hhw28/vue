5028cab26f4f55fce0d77862196748e3221fa840
wanghuan <196912334@qq.com>
2018-10-24 17:50:33 +0800

    add city-router

--- qunaer/src/pages/home/components/Header.vue
+++ qunaer/src/pages/home/components/Header.vue
@@ -5,11 +5,14 @@
     </div>
     <div class="header-input">
       <span class="iconfont">&#xe632;</span>
-      输入城市/景点、游玩主题</div>
-    <div class="header-right ">
-      {{this.city}}
-      <span class="iconfont">&#xe64a;</span>
+      输入城市/景点、游玩主题
     </div>
+    <router-link to="/city">
+      <div class="header-right ">
+        {{this.city}}
+        <span class="iconfont">&#xe64a;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -47,5 +50,6 @@ export default{
       width: 1.24rem
       float: right
       text-align: center
+      color #fff
 
 </style>
