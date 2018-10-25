0c833a569e1ef2e0a080468855f01540946607f0
wanghuan <196912334@qq.com>
2018-10-25 22:55:09 +0800

    header animation

---
+++ qunaer/src/pages/detail/components/Header.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="header">
+    <router-link tag="div" to="/" class="header-abs" v-show="showAbs">
+      <span class="iconfont">&#xe624;</span>
+    </router-link>
+    <div class="header-fixed" v-show="!showAbs" :style="opacityStyle">
+      标题
+      <router-link tag="div" to="/" class="header-fixed-back">
+        <span class="iconfont">&#xe624;</span>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailHeader',
+  data () {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    }
+  },
+  methods: {
+    handleScroll () {
+      const top = document.documentElement.scrollTop
+      if (top > 60) {
+        let opacity = top / 140
+        opacity = opacity > 1 ? 1 : opacity
+        this.opacityStyle = { opacity }
+        this.showAbs = false
+      } else {
+        this.showAbs = true
+      }
+    }
+  },
+  activated () {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header
+    .header-abs
+      position absolute
+      left .2rem
+      top .2rem
+      width .72rem
+      height .72rem
+      border-radius .4rem
+      text-align center
+      color #fff
+      line-height .72rem
+      background rgba(0,0,0,.5)
+    .header-fixed
+      background $bgColor
+      color #fff
+      width 100%
+      height .86rem
+      line-height .86rem
+      font-size .32rem
+      ellipsis()
+      text-align center
+      position fixed
+      top 0
+      left 0
+      .header-fixed-back
+        color #fff
+        position absolute
+        top 0
+        left .16rem
+</style>
